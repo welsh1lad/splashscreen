419110011bc3ed1a61e9055a3a8613bfffa80d23
Welsh1lad <welsh1lad@gmail.com>
2018-08-27 23:31:36 +0100

    I had no terminiating semi colon at the end of the last two lines.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
  print "Running on IP Address ".$_SERVER['REMOTE_ADDR'];
- print "\r\n"
- print "This is running on a openshif webplatform"
- 
+ print "\r\n";
+ print "This is running on a openshif webplatform"; 
 ?>
 
