d2d3b5f716181a376e46c02679143144e563a988
Welsh1lad <welsh1lad@gmail.com>
2018-08-27 23:49:57 +0100

    Correct Ip is incorrect , debugging

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
- print "Running on IP Address ".$_SERVER['REMOTE_ADDR'];
- print " On Openshift Hostname ";
+ print "Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_ADDR'];
+ print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
 ?>
 
