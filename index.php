ab93a47e90e8b80256ad42f1d557d6ab8f8cc3a6
Welsh1lad <welsh1lad@gmail.com>
2018-08-29 00:29:05 +0100

    Display DB Conection message

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
  print "!!! Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
  print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
- print " No connecting to database ";
+ print " Now connecting to database =";
  $mysqli = new mysqli("mysql", "admin", "password", "myphp");
-
+ print ${mysqli};
 ?>
 
