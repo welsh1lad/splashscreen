f8bc89189122fd1c4ad57ed2b037936809033cd9
Welsh1lad <welsh1lad@gmail.com>
2018-08-29 00:36:19 +0100

    chenge env variable

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  print "!!! Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
  print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
- print " Now connecting to database =";
+ print " Now connecting to database = ";
  $mysqli = new mysqli("mysql", "admin", "password", "myphp");
  print ${mysqli};
 ?>
