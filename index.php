15dae4b28eaed74dde53e7f23c27e2e9d19d2ca7
Welsh1lad <welsh1lad@gmail.com>
2018-08-28 23:25:20 +0100

    correct DB Details

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
  print "Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
  print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
+ print " No connecting to database ";
+ $mysqli = new mysqli(${MYSQL_DATABASE}, ${MYSQL_USER}, ${MYSQL_PASSWORD});
+ print $mysqli;
+
 ?>
 
