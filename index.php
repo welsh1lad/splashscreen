ce745e0ecf4b560996843d02c30efebae3134108
Welsh1lad <welsh1lad@gmail.com>
2018-08-29 00:16:53 +0100

    Change loacalhost for ip 127

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
- print "Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
+ print "!!! Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
  print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
  print " No connecting to database ";
- $mysqli = new mysqli(127.0.0.1, "admin", "password", "myphp");
+ $mysqli = new mysqli("127.0.0.1", "admin", "password", "myphp");
 
 ?>
 
