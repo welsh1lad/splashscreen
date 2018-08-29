241b8e46a9be23c4975052f0fd75e09cf4aa726b
Welsh1lad <welsh1lad@gmail.com>
2018-08-29 10:32:04 +0100

    Change mysql connect to mysqli

--- myapp.php
+++ myapp.php
@@ -6,12 +6,10 @@
  $dbroot= getenv("MYSQL_ROOT_PASSWORD");
  $dbdatabase = getenv("MYSQL_DATABASE");
  $dburl = $dbhost . ":" . $dbport;
- $connection = mysql_connect($dburl, $dbuser, $dbpwd);
+ $connection = @mysqli_connect($dburl, $dbuser, $dbpwd);
  print "!!! Running on IP Address ".$_SERVER['SERVER_ADDR']." on port ".$_SERVER['SERVER_PORT'];
  print " And runnin On Openshift Hostname Container ";
  echo gethostname(); 
- print "*******   Now connecting to database = ";
- $connection = mysql_connect($dburl, $dbuser, $dbpwd);
  print $connection;
 ?>
 
